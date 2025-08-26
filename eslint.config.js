c82569245b9b5a12ccbd0e5907aeb1e26654e4df
jubok2-lee <jubok2.lee@samsung.com>
2025-08-26 23:38:02 +0900

    Refactor code style and formatting across multiple files for consistency. Updated .prettierrc, eslint.config.js, index.html, vite.config.ts, App.tsx, and main.tsx to improve readability and maintain coding standards.

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ export default tseslint.config([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+      globals: globals.browser
+    }
+  }
 ])
